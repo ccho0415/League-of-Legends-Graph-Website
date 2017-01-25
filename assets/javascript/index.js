8a8cedba444f1c89c03f935ef170b39ebcbea495
ccho0415 <ccho0415@gmail.com>
2017-01-25 13:10:26 -0500

    testing with riot games api data

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,3 +1,21 @@
+// Test summoner ID : 20278955
 $(document).ready(function(){
-	console.log("hi!")
+	$.ajax({
+  		url: "https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/20278955/recent?api_key=RGAPI-499bc6f3-5fba-4bc5-bc5d-552e71c3c5e3",
+  		method: "GET"
+	}).done(function(data) {
+   		console.log(data);
+   		$.each(data, function(){
+   			$.each(this, function(){
+   				$("#data").append("<p>"+JSON.stringify(this, null, 2)+"</p>");
+   			});
+   		});
+   		
+	});
+	$.ajax({
+		url: "https://na.api.pvp.net/api/lol/na/v1.2/champion?api_key=RGAPI-499bc6f3-5fba-4bc5-bc5d-552e71c3c5e3",
+		method: "GET"
+	}).done(function(data){
+		console.log(data);
+	})
 });
