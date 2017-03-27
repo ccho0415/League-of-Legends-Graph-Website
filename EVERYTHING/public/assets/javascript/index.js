3160ab35be2ce8dce4b3faae6f025dd3a0c7a108
Christine <ccho0415@gmail.com>
2017-03-27 17:38:53 -0400

    bluebird implementation

--- EVERYTHING/public/assets/javascript/index.js
+++ EVERYTHING/public/assets/javascript/index.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
 // Button Functions
   $("body").on("click", ".detail", searchMatch); 
   $("#searchSumName").on("click", searchSummoner);
- 
 // Searching Summoner Name
   function searchSummoner(event){
     summoner = $("#sname").val().trim();    
@@ -19,14 +18,78 @@ $(document).ready(function(){
         return false;
     }else{
       $.post("/data/sumname/"+summoner, function() {
-        console.log("pinging the server")
+        console.log("Searching for a summoner with the name of "+summoner);
       }).then(function(data){
-      console.log(data) 
+        console.log(data)
+          if (data == "Not a summoner"){
+            alert(summoner+" is not a summoner")
+          }else{
+            $("#result").append("<h3>"+data.name+"</h3><br>"+
+            "<img src = 'http://ddragon.leagueoflegends.com/cdn/7.6.1/img/profileicon/"+data.icon+".png'>" )
+            recentGames(data.id);           
+          }
     });
         //============= 
       }
   }
+  function recentGames(id){
+    $.post("/data/recentgames/"+id, function(){
+      console.log("Getting the match history")
+    }).then(function(data){
+      console.log(data)
+      var max = data.gameids.length
+      for (i=0; i < max; i++){
+      var gameMode =data.gamemodes[i];
+      var subType = data.subtypes[i];
+      var gameId = data.gameids[i];
+      var wins = data.wins[i];
+      var kills = data.kills[i];
+      var deaths = data.deaths[i];
+      var assists = data.assists[i];
+      var gold = data.gold[i];
+      var minions = data.minions[i];
+      var champ = data.champs[i]; 
+      var duration;        
+      console.log(gameId);
+      gameDuration(gameId, gameMode, subType, wins, kills, deaths, assists, gold, minions, duration);
+  
 
+      }
+    })
+  }
+  function gameDuration(gameid, gameMode, subType, wins, kills, deaths, assists, gold, minions, duration){
+    return new Promise((resolve, reject)=> {
+      $.post("/data/gameduration/"+ gameid, function(){
+        console.log("Searching for game duration of "+ gameid + " game");
+      }).done(function(data){
+        console.log(data);
+        var duration = parseInt(data) / 60;
+        resolve(duration);
+        console.log(duration)
+        insertGame(gameMode, subType, wins, kills, deaths, assists, gold, minions, duration)        
+      }).fail(function(error){
+        console.log(error);
+        reject(error);
+       });
+   });
+  }
+  function insertGame(gameMode, subType, wins, kills, deaths, assists, gold, minions, duration){
+    if (gameMode == "CLASSIC"){
+      if (subType == "NORMAL" || subType == "RANKED_FLEX_SR" || subType =="RANKED_SOLO_5x5"){
+        $("#data").append("<span>"+ gameMode +" </span>");
+        $("#data").append("<span>"+ subType+ " </span>");
+        $("#data").append("<span> Win: "+wins+" </span>");
+        $("#data").append("<span> KDA : "+ kills + "/" + deaths + "/"+ assists + "</span>");
+        $("#data").append("<span> Gold: "+ gold + " k <img src = 'http://ddragon.leagueoflegends.com/cdn/5.5.1/img/ui/gold.png'> </span>");
+        $("#data").append("<span> Minions : "+ minions + "<img src = 'http://ddragon.leagueoflegends.com/cdn/5.5.1/img/ui/minion.png'> </span>");
+        $("#data").append("<span> Game Duration : "+ duration+ "</span> <br>")                        
+      }else{
+        console.log( "Cannot be analyzed");
+      }
+      }else{
+        console.log( "Cannot be analyzed");                  
+      }       
+  }
   function searchMatch(event){
     var champid = $.parseJSON($(this).attr("data-champid"));
     var matchid = $.parseJSON($(this).attr("data-matchid"))
--- EVERYTHING/routes/data-routes.js
+++ EVERYTHING/routes/data-routes.js
@@ -6,7 +6,7 @@
 // =============================================================
 var request = require('request');
 var $ = require('jQuery');
-
+var Promise = require("bluebird");
 // Routes
 // =============================================================
 module.exports = function(app) {
@@ -14,15 +14,17 @@ module.exports = function(app) {
   // POST route for getting searching summoner by name
   app.post("/data/sumname/:summoner", function(req, res) {
   	var name = req.params.summoner
+  	var name = name.split(' ').join('');
   	var url = "https://na.api.riotgames.com/api/lol/NA/v1.4/summoner/by-name/"+name+"?api_key=RGAPI-499bc6f3-5fba-4bc5-bc5d-552e71c3c5e3"
   	request(url, function(error, response, body){
-  		console.log("=====================================================================================");  		
-  		console.log("Err : "+error);
-  		console.log("=====================================================================================");
-  		console.log("Res : "+response &&response.statusCode);
-  		console.log("=====================================================================================");  		
-  		console.log("Body : "+body);
-   		console.log("====================================================================================="); 		
+  		// Check what type of response is received
+  		// console.log("=====================================================================================");  		
+  		// console.log("Err : "+error);
+  		// console.log("=====================================================================================");
+  		// console.log("Res : "+response &&response.statusCode);
+  		// console.log("=====================================================================================");  		
+  		// console.log("Body : "+body);
+   	// 	console.log("====================================================================================="); 		
   		if (error){
   			error = "Query failed";
    			res.send(error); 			
@@ -31,71 +33,102 @@ module.exports = function(app) {
   			res.send(error);  		
   		} else{
   			info = JSON.parse(body);
-  			console.log(info[name].id);
-  			console.log(info[name].profileIconId);
-  			console.log(info[name].name);  			  			
+   			console.log("=====================================================================================");  			
+  			console.log("Summoner Id : "+info[name].id);
+  			console.log("Profile Icon : "+info[name].profileIconId);
+  			console.log("Summoner Name : "+info[name].name);
+  		   	console.log("=====================================================================================");  	
+  		  	var summoner = {
+  		  			name: info[name].name,
+  		  			icon: info[name].profileIconId,
+  		  			id: info[name].id
+  		  		}
+  		  	res.json(summoner)			  			  			
   		}
 
-  	});
-      // $.ajax({
-      //     url: "https://na.api.riotgames.com/api/lol/NA/v1.4/summoner/by-name/"+req.params.summoner+"?api_key=RGAPI-499bc6f3-5fba-4bc5-bc5d-552e71c3c5e3",
-      //     method: "GET"
-      // }).done(function(data){
-      //     console.log(data);
-      //     $.each(data, function(){
-      //       console.log(this)
-      //       $("#result").append(
-      //       "<h3>"+this.name+"</h3><br>"+
-      //       "<span>"+this.id+"</span><br>"+
-      //      "<img src = 'http://ddragon.leagueoflegends.com/cdn/7.6.1/img/profileicon/"+this.profileIconId+".png'>" )            
-      //         $.ajax({
-      //           url: "https://na.api.riotgames.com/api/lol/NA/v1.3/game/by-summoner/"+this.id+"/recent?api_key=RGAPI-499bc6f3-5fba-4bc5-bc5d-552e71c3c5e3",
-      //           method: "GET"
-      //         }).done(function(data){
-      //           console.log(data)
-      //           $.each(data, function(){
-      //             $.each(this, function(){
-      //                   // Match Duration Code
-      //                   // $.ajax({
-      //                   //   url: "https://na.api.riotgames.com/api/lol/NA/v2.2/match/"+this.gameId+"?includeTimeline=true&api_key=RGAPI-499bc6f3-5fba-4bc5-bc5d-552e71c3c5e3",
-      //                   //   method: "GET"
-      //                   // }).done(function(data){
-      //                   //   $("#data").append("<span>"+data.matchDuration+"</span> <br>")
-      //                   // })
-      //               if(this.gameMode == "CLASSIC" && this.gameType =="MATCHED_GAME"){
-      //                 $("#data").append("<span>"+ this.gameMode +" </span>");
-      //                 $("#data").append("<span>"+ this.subType+ " </span>");
-      //                 let stat = this.stats
-      //                 $("#data").append("<span> Win: "+ stat.win+"</span>")
-      //                 $("#data").append("<span> KDA: "+stat.championsKilled+"/"+stat.numDeaths+"/"+stat.assists+"</button>");         
-      //                 $("#data").append("<span> Gold: "+stat.goldEarned+" k</span>");
-      //                 $("#data").append("<span> Minions: "+stat.minionsKilled+"</span>");
-      //                 $("#data").append("<button data-champid = "+this.championId+" data-matchid="+this.gameId+" class='detail'>Details</button><br>")
-      //               }else{
-      //                 $("#data").append("NOT SR CANT ANALAYZE <br>")
-      //               }
-      //             });
-      //           });                
-      //         })
-      //       })        
-      //   }).fail(function(){
-      //     alert("Could not find summoner "+ summoner);
-      //   });   	
+  	}); 	
   });
 
   // POST route for saving a new todo. We can create a todo using the data on req.body
-  app.post("/api/todos", function(req, res) {
+  app.post("/data/recentgames/:id", function(req, res) {
+  	var sumId = req.params.id
+  	var url = "https://na.api.riotgames.com/api/lol/NA/v1.3/game/by-summoner/"+sumId+"/recent?api_key=RGAPI-499bc6f3-5fba-4bc5-bc5d-552e71c3c5e3"
+  	request(url, function(error,response, body){
+  		// Check What Type of info is received
+  		// console.log("=====================================================================================");  		
+  		// console.log("Err : "+error);
+  		// console.log("=====================================================================================");
+  		// console.log("Res : "+response &&response.statusCode);
+  		// console.log("=====================================================================================");  		
+  		// console.log("Body : "+body);
+   		// console.log("=====================================================================================");
+		var info = JSON.parse(body);
+		// Pretty Print for body
+		// console.log(info);
+  		//  console.log("=====================================================================================");
+   		var gameIdArr = [];
+   		var gameModeArr = [];
+   		var subtypeArr = [];
+   		var winArr = [];
+   		var killsArr = [];
+   		var deathsArr = [];
+   		var assistsArr = [];
+   		var goldArr = [];
+   		var minionsArr = [];
+   		var champsArr = [];		
+		for(i=0; i<info.games.length; i++){
+			var gameid =info.games[i].gameId
+			var stats = info.games[i].stats
+			var gamemode = info.games[i].gameMode
+			var subtype = info.games[i].subType
+			gameIdArr.push(gameid);
+			gameModeArr.push(gamemode);
+			subtypeArr.push(subtype);
+			winArr.push(stats.win);
+			killsArr.push(stats.championsKilled);
+			deathsArr.push(stats.numDeaths);
+			assistsArr.push(stats.assists);
+			goldArr.push(stats.goldEarned);
+			minionsArr.push(stats.minionsKilled);
+			champsArr.push(info.games[i].championId);												
+		}
+   		console.log("=====================================================================================");
+   		var games = {
+   			gameids: gameIdArr,
+   			gamemodes: gameModeArr,
+   			subtypes: subtypeArr,
+   			wins: winArr,
+   			kills: killsArr,
+   			deaths: deathsArr,
+   			assists: assistsArr,
+   			gold: goldArr,
+   			minions: minionsArr,
+   			champs: champsArr	   			
+   		}
+   		console.log(games);
+   		res.json(games);
 
+  	});
   });
 
   // DELETE route for deleting todos. We can access the ID of the todo to delete in
   // req.params.id
-  app.delete("/api/todos/:id", function(req, res) {
-
+  app.post("/data/gameduration/:id", function(req, res) {
+  	var gameid = req.params.id
+  	var url = "https://na.api.riotgames.com/api/lol/NA/v2.2/match/"+gameid+"?includeTimeline=true&api_key=RGAPI-499bc6f3-5fba-4bc5-bc5d-552e71c3c5e3"
+  	request(url, function(error, response, body){
+  		var info = JSON.parse(body);
+  	// Pretty Print Body
+   	// 	console.log("=====================================================================================");  		
+  		// console.log(info);
+   	// 	console.log("=====================================================================================");
+   		var duration = info.matchDuration;
+   		res.json(duration);  		
+  	})
   });
 
   // PUT route for updating todos. We can access the updated todo in req.body
-  app.put("/api/todos", function(req, res) {
+  app.post("/api/todos", function(req, res) {
 
   });
 };
